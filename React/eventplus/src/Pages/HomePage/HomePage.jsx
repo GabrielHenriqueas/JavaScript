abbe92b18fd26b6372890cacfee2ca2b2d18565c
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-11-08 17:20:07 -0300

    08.11.2023

--- React/eventplus/src/Pages/HomePage/HomePage.jsx
+++ React/eventplus/src/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import Title from '../../components/Title/Title';
-import MainContent from '../../components/MainContent/MainContent';
-import Banner from '../../components/Banner/Banner';
-import VisionSection from '../../components/VisionSection/VisionSection';
+import React from "react";
+import "./HomePage.css";
+import MainContent from "../../components/MainContent/MainContent";
+import Banner from "../../components/Banner/Banner";
+import VisionSection from "../../components/VisionSection/VisionSection";
+import ContactSection from "../../components/ContactSection/ContactSection";
+import NextEvent from "../../components/NextEvent/NextEvent";
+import Container from "../../components/Container/Container";
+import Title from "../../components/Title/Title";
 
 const HomePage = () => {
-    return (
-        <MainContent>
-            <Banner/>
-            <VisionSection />
-        </MainContent>
-    );
+  return (
+    <MainContent>
+      <Banner />
+
+      {/* PRÓXIMOS EVENTOS */}
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Próximos Eventos"}/>
+
+          <div className="events-box">
+            <NextEvent
+              title={"Happy Hour Event"}
+              drescription={"Evento Legal"}
+              eventDate={"14/11/2023"}
+              idEvento={"d41b6eb0-e29d-494f-8b38-ac9c3ee48861"}
+            />
+            
+          </div>
+        </Container>
+      </section>
+
+      <VisionSection />
+      <ContactSection />
+    </MainContent>
+  );
 };
 
 export default HomePage;
--- React/eventplus/src/components/VisionSection/VisionSection.jsx
+++ React/eventplus/src/components/VisionSection/VisionSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './VisionSection.css';
 import Title from '../Title/Title';
 
 const VisionSection = () => {
