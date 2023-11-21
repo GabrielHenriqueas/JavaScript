f2313bafee52d5670e1098d130950ab470e93536
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-11-21 17:19:51 -0300

    21.11.2023

--- React/eventplus/src/Pages/TipoEventos/TipoEventos.jsx
+++ React/eventplus/src/Pages/TipoEventos/TipoEventos.jsx
@@ -11,14 +11,20 @@ import { Button, Input } from "../../components/FormComponents/FormComponents";
 
 import api from "../../Services/Service";
 
-import Notification from "../../components/Notification/Notification"
+import Notification from "../../components/Notification/Notification";
+
+import Spinner from "../../components/Spinner/Spinner"
 
 const TipoEventos = () => {
   //satate do componente Notification
-  const [notifyUser, setNotifyUser] = ({});
+  const [notifyUser, setNotifyUser] = useState({});
+  const [showSpinner, setShowSpinner] = useState(false);
 
   const [frmEdit, setFrmEdit] = useState(false);
+
   const [titulo, setTitulo] = useState("");
+  const [idEvento, setIdEvento] = useState(null);//usar apenas para a edição
+
   const [tipoEventos, setTipoEventos] = useState([
     // {"idTipoEvento": "123", "titulo": "Evento ABC"},
     // {"idTipoEvento": "555", "titulo": "Evento xpto"},
@@ -40,9 +46,8 @@ const TipoEventos = () => {
     console.log("A TIPO EVENTOS FOI MONTADA!");
   }, []);
 
-
-
   async function handleSubmit(e) {
+    setShowSpinner(true);
     // parar o submit do formulário
     e.preventDefault();
     // validar pelo menos 3 caracteres
@@ -68,39 +73,73 @@ const TipoEventos = () => {
       console.log("CADASTRO COM SUCESSO!");
       console.log(retorno.data);
       setTitulo(""); //limpa a variável
-
     } catch (error) {
       console.log("Deu ruim na api: ");
       console.log(error);
     }
+    setShowSpinner(false);
   }
   /****************** EDITAR CADASTRO ******************/
 
-  function showUpdateForm() {
-    alert("Mostrando a tela de update");
-  }
+  async function showUpdateForm(idElemento) {
+    setFrmEdit(true);
 
-  async function handleUpdate(id) {
     try {
-      const promise = await api.update(`/TiposEvento/${id}`);
-      console.log(promise.data);
+      // fazer um getById para pegar os dados
+      const retorno = await api.get(`/TiposEvento/${idElemento}`);
+
+      // preencher o título e o id no state
+      setTitulo(retorno.data.titulo);
+      setIdEvento(retorno.data.idTipoEvento);
 
     } catch (error) {
-      alert("Deu ruim na API!");
+      alert("Não foi possível mostrar a tela de edição, tente novamente!");
     }
   }
 
+  async function handleUpdate(e) {
+    try {
+      e.preventDefault();
+
+      // salvar os dados
+      const retorno = await api.put(`/TiposEvento/${idEvento}`, {
+        titulo: titulo
+      });
+
+      setNotifyUser({
+        titleNote: "Sucesso",
+        textNote: `Atualizado com sucesso!`,
+        imgIcon: "success",
+        imgAlt:
+          "Imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação ok.",
+        showMessage: true,
+      });
+
+      // atualizar o state (apiGet)
+      const retornoGet = await api.get(`/TiposEvento`);
+      setTipoEventos(retornoGet.data);// atualiza o state da tabela
+      // limpar o state do título e do idEvento
+      editActionAbort();
+
+    } catch (error) {
+      alert("Problemas na atualização. Verifique a conexão com a internet!");
+    }
+  }
+
+  // reseta o state e cancela a tela de edição
   function editActionAbort() {
-    alert("Cancelar a tela de edição de dados");
+    setFrmEdit(false);
+    setTitulo("");
+    setIdEvento(null);
   }
 
   async function handleDelete(id) {
     try {
       const promise = await api.delete(`/TiposEvento/${id}`);
+      
       const retornoGet = await api.get(`/TiposEvento`);
       console.log(promise.data);
       setTipoEventos(retornoGet.data);
-
     } catch (error) {
       alert("Deu ruim na API!");
     }
@@ -108,7 +147,12 @@ const TipoEventos = () => {
 
   return (
     <MainContent>
-      <Notification {...notifyUser} setNotifyUser={setNotifyUser}/>
+      <Notification {...notifyUser} setNotifyUser={setNotifyUser} />
+      {/* <Spinner /> */}
+      
+      {/* spinner loading condicional */}
+      { showSpinner ? <Spinner /> : null }
+
       {/* Cadastro de tipo eventos */}
       <section className="cadastro-evento-section">
         <Container>
@@ -146,9 +190,45 @@ const TipoEventos = () => {
                   />
                 </>
               ) : (
-                {
-                  /* Atualizar */
-                }(<>Tela de Edição</>)
+                
+                <>
+                    { /* Editar */ }
+                    
+                    <Input
+                      id={"titulo"}
+                      placeholder={"Título"}
+                      name={"titulo"}
+                      type={"text"}
+                      required={"required"}
+                      value={titulo}
+                      manipulationFunction={(e) => {
+                        setTitulo(e.target.value);
+                      }}
+                    />
+
+                    <div className="buttons-editbox">
+
+                    <Button
+                      textButton="Atualizar"
+                      id="atualizar"
+                      name="atualizar"
+                      type="submit"
+                      additionalClass="button-component--middle"
+                    />
+
+                    <Button
+                      textButton="Cancelar"
+                      id="cancelar"
+                      name="cancelar"
+                      type="button"
+                      manipulationFunction={editActionAbort}
+                      additionalClass="button-component--middle"
+                    />
+
+                    </div>
+
+                  </>
+                
               )}
             </form>
           </div>
