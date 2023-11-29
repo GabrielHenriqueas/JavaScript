b67a6fd30f0b13438810bbe09d0366b2035d997c
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-11-29 15:00:28 -0300

    28.11.2023

--- React/eventplus/src/Pages/LoginPage/LoginPage.jsx
+++ React/eventplus/src/Pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,68 @@
-import React from "react";
-import Title from "../../components/Title/Title";
+import React, { useState } from "react";
+import ImageIllustrator from "../../components/ImageIllustrator/ImageIllustrator";
+import logo from "../../assets/images/logo-pink.svg";
+import { Input, Button } from "../../components/FormComponents/FormComponents";
+
+import "./LoginPage.css";
+
+import loginImage from "../../assets/images/login.svg"
 
 const LoginPage = () => {
+  
+  const [user, setUser] = useState({email: "gabriel@admin.com", senha: ""});
+
   return (
-    <div>
-      <Title 
-        titleText={"Página de Login"}
-        additionalClass={'margem-acima'}
-      />
+    <div className="layout-grid-login">
+      <div className="login">
+        <div className="login__illustration">
+          <div className="login__illustration-rotate"></div>
+          <ImageIllustrator
+            imageRender={loginImage}
+            altText="Imagem de um homem em frente de uma porta de entrada"
+            additionalClass="login-illustrator "
+          />
+        </div>
+
+        <div className="frm-login">
+          <img src={logo} className="frm-login__logo" alt="" />
+
+          <form className="frm-login__formbox">
+            <Input
+              additionalClass="frm-login__entry"
+              type="email"
+              id="login"
+              name="login"
+              required={true}
+              value={user.email}
+              manipulationFunction={(e) => {}}
+              placeholder="Username"
+            />
+            <Input
+              additionalClass="frm-login__entry"
+              type="password"
+              id="senha"
+              name="senha"
+              required={true}
+              value={user.senha}
+              manipulationFunction={(e) => {}}
+              placeholder="****"
+            />
+
+            <a href="" className="frm-login__link">
+              Esqueceu a senha?
+            </a>
+
+            <Button
+              buttonText="Login"
+              id="btn-login"
+              name="btn-login"
+              type="submit"
+              additionalClass="frm-login__button"
+              onClick={()=>{}}
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
