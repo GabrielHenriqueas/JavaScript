bdafac6d516294c506cb0e63820109b7a7992af4
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-12-01 17:05:52 -0300

    01.12.2023

--- React/eventplus/src/Routes/routes.js
+++ React/eventplus/src/Routes/routes.js
@@ -1,27 +1,53 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from '../Pages/HomePage/HomePage';
-import TipoEventos from '../Pages/TipoEventos/TipoEventos';
-import EventosPage from '../Pages/EventosPage/EventosPage';
-import LoginPage from '../Pages/LoginPage/LoginPage'
-import TestePage from '../Pages/TestePage/TestePage';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../Pages/HomePage/HomePage";
+import TipoEventos from "../Pages/TipoEventos/TipoEventos";
+import EventosPage from "../Pages/EventosPage/EventosPage";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import TestePage from "../Pages/TestePage/TestePage";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import { PrivateRoute } from "./PrivateRoute";
 
 const Rotas = () => {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route element={<HomePage />} path='/' exact />
-                <Route element={<TipoEventos />} path='/tipo-eventos' />
-                <Route element={<EventosPage />} path='/eventos' />
-                <Route element={<LoginPage />} path='/login' />
-                <Route element={<TestePage />} path='/testes' />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route element={<HomePage />} path="/" exact />
+        <Route
+          path="/tipo-eventos"
+          element={
+            <PrivateRoute redirectTo="/">
+              <TipoEventos />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/eventos"
+          element={
+            <PrivateRoute redirectTo="/">
+              <EventosPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/eventos-alunos"
+          element={
+            <PrivateRoute redirectTo="/">
+              <EventosPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<TestePage />} path="/testes" />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default Rotas;
--- React/eventplus/src/components/Nav/Nav.jsx
+++ React/eventplus/src/components/Nav/Nav.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
 import logoMobile from "../../assets/images/logo-white.svg";
 import logoDesktop from "../../assets/images/logo-pink.svg";
+import { UserContext } from "../../assets/context/AuthContext";
 
 const Nav = ({  exibeNavbar , setExibeNavbar}) => {
+
+  const {userData, setUserData} = useContext(UserContext);
+
   return (
     <nav className={`navbar ${exibeNavbar ? "exibeNavbar" : ""}`}>
       <span
@@ -27,9 +31,22 @@ const Nav = ({  exibeNavbar , setExibeNavbar}) => {
 
       <div className="navbar__items-box">
         <Link to="/" className="navbar__item">Home</Link>
-        <Link to="/tipo-eventos" className="navbar__item">Tipo de eventos</Link>
+        {/* <Link to="/tipo-eventos" className="navbar__item">Tipo de eventos</Link>
         <Link to="/eventos" className="navbar__item">Eventos</Link>
-        <Link to="/login" className="navbar__item">Login</Link>
+        <Link to="/eventos-aluno" className="navbar__item">Eventos Alunos</Link> */}
+        {userData.role === "admin" ? (
+            <>
+              <Link to="/tipo-eventos" className="navbar__item">Tipos Eventos</Link>
+              <Link to="/eventos" className="navbar__item">Eventos</Link>
+            </>
+        ) : (
+              userData.role === "Comum" ? (
+              
+              <Link to="/eventos-alunos" className="navbar__item">Eventos Aluno</Link>
+
+            ) : (null)
+        )}
+        {/* <Link to="/login" className="navbar__item">Login</Link> */}
         {/* <Link to="/testes" className="navbar__item">Teste</Link> */}
       </div>
     </nav>
